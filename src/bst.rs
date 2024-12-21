64a4e3e08600e5edb36555ac7139003fb264ce14
light-ly <l136665@163.com>
2024-12-22 02:02:44 +0800

    add bst insert and basic test

--- src/bst.rs
+++ src/bst.rs
@@ -1,1 +1,43 @@
-struct Bst;
+struct BstNode<T> {
+    data: T,
+    left: Option<Box<BstNode<T>>>,
+    right: Option<Box<BstNode<T>>>
+}
+
+impl <T: PartialOrd> BstNode<T> {
+    fn new(data: T) -> Self {
+        BstNode { data, left: None, right: None }
+    }
+
+    fn insert(&mut self, data: T) {
+        if data < self.data {
+            match &mut self.left {
+                Some(left) => left.insert(data),
+                None => self.left = Some(Box::new(Self::new(data)))
+            }
+        } else {
+            match &mut self.right {
+                Some(left) => left.insert(data),
+                None => self.left = Some(Box::new(Self::new(data)))
+            }
+        }
+    }
+}
+
+pub struct Bst<T> {
+    root: Option<BstNode<T>>
+}
+
+impl<T: PartialOrd> Bst<T> {
+    pub fn new() -> Self {
+        Bst { root: None }
+    }
+
+    pub fn insert(&mut self, data: T) {
+        if let Some(root) = &mut self.root {
+            root.insert(data);
+        } else {
+            self.root = Some(BstNode::<T>::new(data))
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use rand::Rng;
+
 mod bst;
 
+use bst::Bst;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, here is LiuYi's algorithm exercise!");
+    let mut rng = rand::thread_rng();
+    let test_vector = (0..18).map(|_| rng.gen_range(0..=1000)).collect();
+    bst_test(test_vector);
+}
+
+fn bst_test(test_vector: Vec<u64>) {
+    let mut bst = Bst::<u64>::new();
+
+    for ele in test_vector {
+        bst.insert(ele);
+    }
 }
